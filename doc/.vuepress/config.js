10b7b28d5152d7cf93c19c4eb09dd66ee5457b84
Venkat Peri <venkatperi@gmail.com>
2019-10-28 08:46:14 +0530

    docs & vuepress

---
+++ doc/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "StateM",
+  base: "/statem/",
+  dest: "docs",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Demo", link: "/demo/" },
+      { text: "Github", link: "https://github.com/venkatperi/statem" }
+    ],
+
+    sidebar: [
+      "/"
+    ]
+
+  }
+}
